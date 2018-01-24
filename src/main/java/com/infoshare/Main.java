0ca85d4ba244cdcf9890269444cca8e5a65ebd69
Krzysztof Syska <prezydium834@gmail.com>
2018-01-24 11:24:09 +0100

    Second commit

--- src/main/java/com/infoshare/Main.java
+++ src/main/java/com/infoshare/Main.java
@@ -3,7 +3,13 @@ package com.infoshare;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("infoShareAcademy - Java SE");
+       // System.out.println("infoShareAcademy - Java SE");
+
+        Menu menu1 = new Menu(23, "costamcostam");
+
+
+        menu1.showParams();
+        menu1.checkText();
     }
 }
 
