d52520823d3c53d3e23aeb50c20f3be66bb08fb9
Krzysztof Syska <prezydium834@gmail.com>
2018-01-24 13:54:55 +0100

    Learning if's

--- src/main/java/com/infoshare/Main.java
+++ src/main/java/com/infoshare/Main.java
@@ -7,8 +7,12 @@ public class Main {
        // System.out.println("infoShareAcademy - Java SE");
 
         Menu menu1 = new Menu();
-       // menu1.setNumber(123);
-      //  menu1.setText("Oliwa");
+        menu1.setNumber(1);
+        Menu menu2 = new Menu();
+        menu2.setNumber(2);
+        menu1.setCityByNumber();
+        menu2.setCityByNumber();
+    /**  menu1.setText("Oliwa");
 
         //menu1.showParams();
         //menu1.checkText();
@@ -21,9 +25,36 @@ public class Main {
 
         System.out.println(menu1.getZ());
 
-        menu1.setCity1(Cities.GDAŃSK);
+        menu1.setCity(Cities.GDAŃSK);
 
         System.out.println(menu1.getCity());
+
+        int liczbaA = 10;
+        int liczbaB = 3;
+
+         double wynikDouble1 = (double) liczbaA / liczbaB;
+
+        System.out.println(wynikDouble1);
+
+
+        double liczbaC = (double) liczbaA;
+        double liczbaD = (double) liczbaB;
+
+        double wynikDouble = liczbaC / liczbaD;
+
+        System.out.println(wynikDouble);
+
+        int i = 3;
+
+        System.out.println(i++);
+        System.out.println(++i); **/
+
+        System.out.println(menu1.getCity());
+        System.out.println(menu2.getCity());
+
+
+
+
     }
 }
 
