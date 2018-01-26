bde0a6f08c6cc709c2c94d5f56e14749337ac49a
Krzysztof Syska <prezydium834@gmail.com>
2018-01-26 14:22:24 +0100

    Abstract classess and methods

---
+++ src/main/java/com/infoshare/AbstractMenu.java
@@ -0,0 +1,7 @@
+package com.infoshare;
+
+public abstract class AbstractMenu {
+    public abstract void show();
+    public abstract void close();
+    public abstract void runOption(int option);
+}
